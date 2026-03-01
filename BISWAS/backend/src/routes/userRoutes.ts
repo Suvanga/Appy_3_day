6f5f84383603abd94777cf2fc3710d377f96a944
Suvanga <83526481+Suvanga@users.noreply.github.com>
2026-03-01 17:57:47 -0500

    New user controller methods are added for CRUD

--- BISWAS/backend/src/routes/userRoutes.ts
+++ BISWAS/backend/src/routes/userRoutes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
-import { createOrGetUser } from '../controllers/user.Controller';
-import { deleteUser } from '../controllers/user.Controller';
+import { 
+  createOrGetUser, 
+  deleteUser, 
+  getCurrentUser, 
+  updateUser 
+} from '../controllers/user.Controller';
+import { checkJwt } from '../middleware/authMiddleware';
+
+
 const router = Router();
 
 // Route: POST /api/users
 // Description: Receives user data from frontend to login or register
-router.post('/', createOrGetUser);
-router.delete('/:auth0_id', deleteUser);
+router.post('/', checkJwt, createOrGetUser);
+router.delete('/:auth0_id', checkJwt, deleteUser);
+router.get('/me', checkJwt, getCurrentUser);
+router.put('/me', checkJwt, updateUser);
+
 
 export default router;
