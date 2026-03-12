b2c2df9dae393a8b5308fab0b68f58bc4269b342
Suvanga <83526481+Suvanga@users.noreply.github.com>
2026-03-12 10:46:42 -0400

    chore: trigger initial AWS deployment

--- BISWAS/backend/src/server.ts
+++ BISWAS/backend/src/server.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+//Trigerring the docker code comment 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
